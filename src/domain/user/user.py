579bfd4d246d131835f895b85217e86b71bd0435
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-15 20:14:04 +0900

    ✨ feat(user): 비밀번호 변경 메서드 추가
    - User 클래스에 비밀번호 변경을 위한 change_password 메서드를 추가하여, 새로운 비밀번호를 해싱하고 업데이트하는 기능을 구현함
    - 이를 통해 사용자 비밀번호 관리의 유연성을 높임

--- src/domain/user/user.py
+++ src/domain/user/user.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from domain.base.aggregate import Aggregate
 from domain.user.value_objects import Email, HashedPassword, PlainPassword, UserName
 from shared_kernel.hasher.hasher import Hasher
+from shared_kernel.time_provider.time_provider import TimeProvider
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -13,3 +14,11 @@ class User(Aggregate):
 
     def authenticate(self, plain_password: PlainPassword, hasher: Hasher) -> bool:
         return hasher.verify(plain_password.value, self.hashed_password.value)
+
+    def change_password(
+        self, time_provider: TimeProvider, plain_password: PlainPassword, hasher: Hasher
+    ) -> None:
+        new_hashed_password: HashedPassword = HashedPassword(
+            hasher.hash(plain_password.value)
+        )
+        self.update(time_provider=time_provider, hashed_password=new_hashed_password)
