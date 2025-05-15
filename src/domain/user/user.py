60a620650c27e8d438b48a0caea2657bb7e82ed3
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-15 20:09:16 +0900

    ✨ feat(user): 사용자 인증 메서드 추가
    - User 클래스에 비밀번호 검증을 위한 authenticate 메서드를 추가하여, Hasher를 사용한 비밀번호 인증 기능을 구현함
    - 이를 통해 사용자 인증 과정의 보안성을 강화함

--- src/domain/user/user.py
+++ src/domain/user/user.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 
 from domain.base.aggregate import Aggregate
-from domain.user.value_objects import Email, HashedPassword, UserName
+from domain.user.value_objects import Email, HashedPassword, PlainPassword, UserName
+from shared_kernel.hasher.hasher import Hasher
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -9,3 +10,6 @@ class User(Aggregate):
     name: UserName
     email: Email
     hashed_password: HashedPassword
+
+    def authenticate(self, plain_password: PlainPassword, hasher: Hasher) -> bool:
+        return hasher.verify(plain_password.value, self.hashed_password.value)
