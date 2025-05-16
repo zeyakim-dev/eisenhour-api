a81d6b5e034f4dbccbf7b8569b2d43dda61c1f85
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-16 12:39:53 +0900

    ✨ feat(command): 명령 처리기 및 명령 클래스 추가
    - 명령 처리기(CommandHandler)와 명령(Command) 클래스를 새로 추가하여, 명령 기반 아키텍처를 위한 기초를 마련함
    - Command 클래스는 고유 ID와 생성 시간을 포함하며, CommandHandler는 명령을 실행하는 추상 메서드를 정의함

---
+++ src/application/messaging/command/base/command.py
@@ -0,0 +1,18 @@
+from abc import ABC
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Any, Self
+from uuid import UUID, uuid4
+
+from domain.shared.time_provider import TimeProvider
+
+
+@dataclass(frozen=True, kw_only=True)
+class Command(ABC):
+    id: UUID = field(default_factory=uuid4)
+    created_at: datetime
+
+    @classmethod
+    def create(cls, time_provider: TimeProvider, **kwargs: Any) -> Self:
+        kwargs.setdefault("created_at", time_provider.now())
+        return cls(**kwargs)
