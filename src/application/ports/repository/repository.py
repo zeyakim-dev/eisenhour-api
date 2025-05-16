3d18f3a6662eb8106761785735aa0660e8844dd3
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-16 15:53:28 +0900

    ♻️ refactor(repository): 제네릭을 사용한 리포지토리 클래스 개선
    - Repository 클래스에 제네릭 타입을 도입하여, 다양한 엔티티 타입을 지원하도록 수정함
    - save, get, _save, _get 메서드의 매개변수 타입을 제네릭으로 변경하여 코드의 유연성을 높임

--- src/application/ports/repository/repository.py
+++ src/application/ports/repository/repository.py
@@ -1,22 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
 from uuid import UUID
 
 from domain.base.entity import Entity
 
+E = TypeVar("E", bound=Entity)
 
-class Repository(ABC):
-    def save(self, entity: Entity) -> None:
+
+class Repository(ABC, Generic[E]):
+    def save(self, entity: E) -> None:
         self._save(entity)
 
     @abstractmethod
-    def _save(self, entity: Entity) -> None:
+    def _save(self, entity: E) -> None:
         pass
 
-    def get(self, id: UUID) -> Entity:
+    def get(self, id: UUID) -> E:
         return self._get(id)
 
     @abstractmethod
-    def _get(self, id: UUID) -> Entity:
+    def _get(self, id: UUID) -> E:
         pass
 
     def delete(self, id: UUID) -> None:
