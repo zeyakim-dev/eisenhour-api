7d8798a89261d09d0de54b2280b196100879688b
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-16 21:55:35 +0900

    ✅ test: 사용자 등록 테스트용 Fake 객체 및 fixture 구성
    - FakeTimeProvider, FakeHasher, FakeUserEntity, FakeUserInMemoryRepository 클래스 추가
    - pytest fixture를 통해 테스트 데이터 및 환경 구성
    - 사용자 등록 테스트를 위한 유효한 사용자 데이터 생성 기능 구현

---
+++ tests/unit/conftest.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from uuid import UUID, uuid4
+
+import pytest
+
+from application.ports.repository.exceptions import EntityNotFoundError
+from domain.user.repository.exceptions import (
+    EmailAlreadyExistsError,
+    UsernameAlreadyExistsError,
+)
+
+
+class FakeTimeProvider:
+    def __init__(self) -> None:
+        pass
+
+    def now(self):
+        return datetime.now()
+
+
+@pytest.fixture
+def time_provider():
+    return FakeTimeProvider()
+
+
+class FakeHasher:
+    def hash(self, password: str) -> str:
+        return "hashed_" + password
+
+    def verify(self, password: str, hashed_password: str) -> bool:
+        return "hashed_" + password == hashed_password
+
+
+@pytest.fixture
+def hasher():
+    return FakeHasher()
+
+
+@dataclass(frozen=True, kw_only=True)
+class FakeUserEntity:
+    id: UUID = field(default_factory=uuid4)
+
+    created_at: datetime = field(default_factory=datetime.now())
+    username: str
+    email: str
+    password: str
+
+
+@pytest.fixture
+def valid_user1():
+    return FakeUserEntity(
+        username="test_user1", email="test1@example.com", password="hashed_Test_pw_1!"
+    )
+
+
+@pytest.fixture
+def valid_user2():
+    return FakeUserEntity(
+        username="test_user2", email="test2@example.com", password="hashed_Test_pw_2!"
+    )
+
+
+class FakeUserInMemoryRepository:
+    def __init__(self, items: dict[UUID, FakeUserEntity] | None = None):
+        self.items = items or {}
+
+    def save(self, entity: FakeUserEntity) -> None:
+        self.items[entity.id] = entity
+
+    def get(self, id: UUID) -> FakeUserEntity:
+        try:
+            return self.items[id]
+        except KeyError:
+            raise EntityNotFoundError(self.__class__.__name__, id)
+
+    def delete(self, id: UUID) -> None:
+        try:
+            del self.items[id]
+        except KeyError:
+            raise EntityNotFoundError(self.__class__.__name__, id)
+
+    def check_username_exists(self, username: str) -> None:
+        if any(user.username == username for user in self.items.values()):
+            raise UsernameAlreadyExistsError()
+
+    def check_email_exists(self, email: str) -> None:
+        if any(user.email == email for user in self.items.values()):
+            raise EmailAlreadyExistsError()
+
+
+@pytest.fixture
+def fake_user_inmemory_repository(
+    valid_user1: FakeUserEntity, valid_user2: FakeUserEntity
+):
+    items = {valid_user1.id: valid_user1, valid_user2.id: valid_user2}
+    return FakeUserInMemoryRepository(items=items)
