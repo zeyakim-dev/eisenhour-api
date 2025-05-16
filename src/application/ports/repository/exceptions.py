49d9cb6f2c3f6275356df40c6eb67974e3f5816e
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-17 01:02:54 +0900

    📝 docs(repository, exceptions): 예외 클래스 및 저장소 추상 클래스의 docstring 추가 및 개선
    - EntityNotFoundError 클래스에 대한 docstring을 추가하여 예외 발생 조건 및 설명을 명확히 함
    - Repository 추상 클래스에 대한 docstring을 추가하여 저장소의 역할과 메서드 설명을 보완함
    - 코드 가독성과 문서화 수준 향상

--- src/application/ports/repository/exceptions.py
+++ src/application/ports/repository/exceptions.py
@@ -1,4 +1,15 @@
 class EntityNotFoundError(Exception):
+    """엔터티 조회 실패에 대한 예외입니다.
+
+    저장소에서 주어진 ID를 가진 엔터티를 찾지 못했을 때 발생합니다.
+    """
+
     def __init__(self, repository_name: str, id: str):
+        """예외 메시지를 설정하여 초기화합니다.
+
+        Args:
+            repository_name (str): 엔터티를 조회한 저장소의 이름.
+            id (str): 조회 대상 엔터티의 식별자.
+        """
         self.message = f"{id} not found in {repository_name}"
         super().__init__(self.message)
