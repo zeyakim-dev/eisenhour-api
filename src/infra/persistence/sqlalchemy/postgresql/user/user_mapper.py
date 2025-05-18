5140ce817b258d1e3bbfb64516f5be4abf9c9f16
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-18 15:02:46 +0900

    ✨ feat(user_mapper): User 엔터티와 UserModel 간 매핑 클래스 추가
    - UserMapper 클래스를 구현하여 User 엔터티와 UserModel 간의 변환 로직을 정의
    - to_entity 및 to_model 메서드를 통해 데이터 변환 기능 제공

---
+++ src/infra/persistence/sqlalchemy/postgresql/user/user_mapper.py
@@ -0,0 +1,28 @@
+from domain.user.user import User
+from domain.user.value_objects import Email, HashedPassword, Username
+from infra.persistence.base.mapper import Mapper
+from infra.persistence.sqlalchemy.postgresql.user.user_model import UserModel
+
+
+class UserMapper(Mapper[User, UserModel]):
+    """User 엔터티와 UserModel 간 매핑을 수행하는 매퍼 클래스."""
+
+    def to_entity(self, model: UserModel) -> User:
+        return User(
+            id=model.id,
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+            username=Username(model.username),
+            email=Email(model.email),
+            hashed_password=HashedPassword(model.hashed_password),
+        )
+
+    def to_model(self, entity: User) -> UserModel:
+        return UserModel(
+            id=entity.id,
+            created_at=entity.created_at,
+            updated_at=entity.updated_at,
+            username=entity.username.value,
+            email=entity.email.value,
+            hashed_password=entity.hashed_password.value,
+        )
