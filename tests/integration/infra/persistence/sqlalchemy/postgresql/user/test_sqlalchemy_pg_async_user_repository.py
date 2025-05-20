b39ea856a94f0ac40d5e3b6a68471a0bbd8812fe
zeyakim-dev <zeyakimdev@gmail.com>
2025-05-20 21:28:58 +0900

    ✅ test(user_repository): 비동기 SQLAlchemy 사용자 저장소에 대한 통합 테스트 추가
    - SQLAlchemyPGAsyncUserRepository에 대한 새로운 통합 테스트 파일 생성
    - 사용자 이름 및 이메일 존재 여부 확인을 위한 테스트 케이스 구현
    - 데이터베이스 준비 및 세션 관리를 위한 pytest_asyncio 기능 사용

---
+++ tests/integration/infra/persistence/sqlalchemy/postgresql/user/test_sqlalchemy_pg_async_user_repository.py
@@ -0,0 +1,100 @@
+from datetime import timedelta, timezone
+
+import pytest
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from domain.user.repository.exceptions import (
+    EmailAlreadyExistsError,
+    UsernameAlreadyExistsError,
+)
+from domain.user.user import User
+from domain.user.value_objects import Email, HashedPassword, Username
+from shared_kernel.time.time_provider import TimeProvider
+from src.infra.persistence.sqlalchemy.postgresql.user.user_mapper import UserMapper
+from src.infra.persistence.sqlalchemy.postgresql.user.user_repository import (
+    SQLAlchemyPGAsyncUserRepository,
+)
+
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def prepare_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(User.metadata.drop_all)
+        await conn.run_sync(User.metadata.create_all)
+    yield
+
+
+@pytest_asyncio.fixture
+async def db_session(prepare_db):
+    async with AsyncSessionLocal() as session:
+        await session.begin()  # 외부 트랜잭션
+        await session.begin_nested()  # SAVEPOINT
+        yield session
+        await session.rollback()  # 테스트 후 롤백
+
+
+@pytest.fixture
+def time_provider():
+    tz_kst = timezone(timedelta(hours=9))
+    return TimeProvider(tz_kst)
+
+
+@pytest.fixture
+def test_user(time_provider: TimeProvider):
+    return User.create(
+        time_provider=time_provider,
+        username=Username("test_user"),
+        email=Email("test@test.com"),
+        hashed_password=HashedPassword("Test_pw_123!"),
+    )
+
+
+@pytest_asyncio.fixture
+async def user_repository(test_user: User, db_session: AsyncSession):
+    user_repository = SQLAlchemyPGAsyncUserRepository(
+        session_factory=AsyncSessionLocal,
+        mapper=UserMapper(),
+    )
+    db_session.add(test_user)
+    await db_session.flush()
+
+    return user_repository
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio
+class TestSqlalchemyPgAsyncUserRepository:
+    async def test_check_username_exists_raise_exception_when_username_exists(
+        self,
+        user_repository: SQLAlchemyPGAsyncUserRepository,
+        test_user: User,
+    ):
+        with pytest.raises(UsernameAlreadyExistsError):
+            await user_repository.check_username_exists(test_user.username)
+
+    async def test_check_username_exists_returns_none_when_username_not_exists(
+        self,
+        user_repository: SQLAlchemyPGAsyncUserRepository,
+    ):
+        new_username = "new_username"
+        assert await user_repository.check_username_exists(new_username) is None
+
+    async def test_check_email_exists_raise_exception_when_email_exists(
+        self,
+        user_repository: SQLAlchemyPGAsyncUserRepository,
+        test_user: User,
+    ):
+        with pytest.raises(EmailAlreadyExistsError):
+            await user_repository.check_email_exists(test_user.email)
+
+    async def test_check_email_exists_returns_none_when_email_not_exists(
+        self,
+        user_repository: SQLAlchemyPGAsyncUserRepository,
+    ):
+        new_email = "new_email@test.com"
+        assert await user_repository.check_email_exists(new_email) is None
